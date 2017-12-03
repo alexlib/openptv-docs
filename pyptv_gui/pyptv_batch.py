9dfbb4b068892d6870a18975563acade9d8cac08
Alex Liberzon <alex.liberzon@gmail.com>
2017-12-04 00:44:45 +0200

    working on the pyptv_batch slowly

--- pyptv_gui/pyptv_batch.py
+++ pyptv_gui/pyptv_batch.py
@@ -15,6 +15,7 @@ the present "active" parameters are kept intact except the sequence
 import os
 import sys
 import numpy as np
+from skimage.io import imread
 
 # project specific inputs
 import parameters as par
@@ -29,122 +30,125 @@ cwd = os.getcwd()
 
 # import pdb; pdb.set_trace()
 
+from optv.correspondences import correspondences, MatchedCoords
+from optv.segmentation import target_recognition
+from optv.orientation import point_positions
+from optv.image_processing import preprocess_image
+from optv.tracking_framebuf import CORRES_NONE
+from optv.tracker import Tracker
+from optv.calibration import Calibration
+from optv.parameters import ControlParams, VolumeParams, TrackingParams, \
+    SequenceParams
+
+def simple_highpass(img, cpar):
+    return preprocess_image(img, 0, cpar, 12)
 
 
-def sequence_tracking(n_img):
-    # get following variables from the parameters:
-    # n_camera, seq_first, seq_last, base_name
-    import ptv1 as ptv
-    
-    sequenceParams = par.SequenceParams(n_img, path=par.temp_path)
-    sequenceParams.read()
-    (base_name, seq_first, seq_last) = (
-        sequenceParams.base_name, sequenceParams.first, sequenceParams.last)
-
-    print ("Starting sequence action")
-
-    ptv.py_sequence_init(0)
-    stepshake = ptv.py_get_from_sequence_init()
-    if not stepshake:
-        stepshake = 1
-    print stepshake
-    temp_img = np.array([], dtype=np.ubyte)
-    for i in range(seq_first, seq_last + 1, stepshake):
-        if i < 10:
-            seq_ch = "%01d" % i
-        elif i < 100:
-            seq_ch = "%02d" % i
-        else:
-            seq_ch = "%03d" % i
-        for j in range(n_img):
-            img_name = base_name[j] + seq_ch
-            print ("Setting image: ", img_name)
-            try:
-                temp_img = imread(img_name).astype(np.ubyte)
-            except:
-                print "Error reading file"
-
-            ptv.py_set_img(temp_img, j)
-
-        ptv.py_sequence_loop(0, i)
-
-
-#	forward tracking
-    run_info = ptv.py_trackcorr_init()
-    print run_info.get_sequence_range()
-    for step in range(*run_info.get_sequence_range()):
-        print step
-        ptv.py_trackcorr_loop(run_info, step, display=0)
-
-    ptv.py_trackcorr_finish(run_info, step + 1)
-    print "tracking without display finished"
-    # RON - cancled back tracking due to bug 
-    ptv.py_trackback_c()
-    print "tracking backwards is finished"
-
-
-def sequence(n_img):
-    # get following variables from the parameters:
-    # n_camera, seq_first, seq_last, base_name
-    import ptv1 as ptv
-    
-    sequenceParams = par.SequenceParams(n_img, path=par.temp_path)
-    sequenceParams.read()
-    (base_name, seq_first, seq_last) = (
-        sequenceParams.base_name, sequenceParams.first, sequenceParams.last)
-
-    print ("Starting sequence action")
-
-    ptv.py_sequence_init(0)
-    stepshake = ptv.py_get_from_sequence_init()
-    if not stepshake:
-        stepshake = 1
-    print stepshake
-    temp_img = np.array([], dtype=np.ubyte)
-    for i in range(seq_first, seq_last + 1, stepshake):
-        if i < 10:
-            seq_ch = "%01d" % i
-        elif i < 100:
-            seq_ch = "%02d" % i
-        else:
-            seq_ch = "%03d" % i
-        for j in range(n_img):
-            img_name = base_name[j] + seq_ch
-            print ("Setting image: ", img_name)
-            try:
-                temp_img = imread(img_name).astype(np.ubyte)
-            except:
-                print "Error reading file"
-
-            ptv.py_set_img(temp_img, j)
-
-        ptv.py_sequence_loop(0, i)
 
 
 def run_batch(new_seq_first, new_seq_last):
-    #  	import pdb; pdb.set_trace()
-    import ptv1 as ptv
+    """ this file runs inside exp_path, so the other names are
+    prescribed by the OpenPTV type of a folder:
+        /parameters
+        /img
+        /cal
+        /res
+    """
+    # read the number of cameras
+    with open('parameters/ptv.par','r') as f:
+        n_cams = int(f.readline())
     
-    ptv.py_init_proc_c()
-    ptv.py_start_proc_c()  # or ptv.py_init_proc_c()?
-    ptvParams = par.PtvParams(path=par.temp_path)
-    ptvParams.read()
-    (n_img, img_name, img_cal, hp_flag, allCam_flag, tiff_flag, imx, imy, pix_x, pix_y, chfield, mmp_n1, mmp_n2, mmp_n3, mmp_d) = \
-        (ptvParams.n_img, ptvParams.img_name, ptvParams.img_cal, ptvParams.hp_flag, ptvParams.allCam_flag, ptvParams.tiff_flag,
-         ptvParams.imx, ptvParams.imy, ptvParams.pix_x, ptvParams.pix_y, ptvParams.chfield, ptvParams.mmp_n1, ptvParams.mmp_n2, ptvParams.mmp_n3, ptvParams.mmp_d)
-# read the sequence parameters
-    sequenceParams = par.SequenceParams(n_img, path=par.temp_path)
-    sequenceParams.read()
-    (base_name, seq_first, seq_last) = (
-        sequenceParams.base_name, sequenceParams.first, sequenceParams.last)
-# write the new sequence parameters
-    par.SequenceParams(n_img, base_name,
-                       new_seq_first, new_seq_last, path=par.temp_path).write()
-    # if you need sequence and tracking:
-    sequence_tracking(n_img)
+    # Control parameters
+    cpar = ControlParams(n_cams)
+    cpar.read_control_par('parameters/ptv.par')
+    
+    # Sequence parameters
+    spar = SequenceParams(num_cams=n_cams)
+    spar.read_sequence_par('parameters/sequence.par',n_cams)
+    spar.set_first(new_seq_first)
+    spar.set_last(new_seq_last)
+    
+    # Volume parameters
+    vpar = VolumeParams()
+    vpar.read_volume_par('parameters/criteria.par')
+    
+    # Target parameters
+    tpar = TrackingParams()
+    tpar.read_track_par('parameters/track.par')
+    
+    # Calibration parameters
+    cals =[]
+    for i in xrange(n_cams):
+        cal = Calibration()
+        cals.append(cal.from_file(ori_file, addpar_file))
+    
+    
+    # sequence
+    for frame in xrange(new_seq_first, new_seq_last+1):
+        print "processing frame %d" % frame
+        
+        detections = []
+        corrected = []
+        for i_cam in xrange(n_cams):
+            imname = spar.get_img_base_name(i_cam) + str(frame)
+            img = imread(imname)
+            hp = simple_highpass(img, cpar)
+            targs = target_recognition(hp, tpar, i_cam, cpar)
+            targs.sort_y()
+            detections.append(targs)
+            corrected.append(MatchedCoords(targs, cpar, cals[i_cam]))
+        
+        if any([len(det) == 0 for det in detections]):
+            return False
+        
+        # Corresp. + positions.
+        sets, corresp, _ = correspondences(
+            detections, corrected, cals, vpar, cpar)
+        
+        # Save targets only after they've been modified:
+        for i in xrange(n_cams):
+            detections[i].write(imname+'_targets')
+        
+        if self._report:
+            print "Frame " + str(frame) + " had " \
+            + repr([s.shape[1] for s in sets]) + " correspondences."
+        
+        # Distinction between quad/trip irrelevant here.
+        sets = np.concatenate(sets, axis=1)
+        corresp = np.concatenate(corresp, axis=1)
+        
+        flat = np.array([corrected[cix].get_by_pnrs(corresp[cix]) \
+            for cix in xrange(len(self._cals))])
+        pos, rcm = point_positions(
+            flat.transpose(1,0,2), self._cpar, self._cals)
+        
+        # Save rt_is
+        rt_is = open(seq['output_template'].format(frame=frame), 'w')
+        rt_is.write(str(pos.shape[0]) + '\n')
+        for pix, pt in enumerate(pos):
+            pt_args = (pix + 1,) + tuple(pt) + tuple(corresp[:,pix])
+            rt_is.write("%4d %9.3f %9.3f %9.3f %4d %4d %4d %4d\n" % pt_args)
+        rt_is.close()
+    
+    
+    # tracker
+    cals = []
+    img_base = []
+    for cix, cam_spec in enumerate(yaml_conf['cameras']):
+        cam_spec.setdefault('addpar_file', None)
+        cal = Calibration()
+        cal.from_file(cam_spec['ori_file'], cam_spec['addpar_file'])
+        cals.append(cal)
+        img_base.append(seq_cfg['targets_template'].format(cam=cix + 1))
+        
 
-    # if you need sequence only:
-    # sequence(n_img)
+    
+    tracker = Tracker(cpar, vpar, tpar, spar, cals, framebuf_naming)
+    tracker.full_forward()
+    
+
+
+
     
 def main(sys_argv, repetitions=1):
     """ runs the batch 
@@ -170,7 +174,6 @@ def main(sys_argv, repetitions=1):
     src_path = string.replace(software_path,'pyptv_gui','src_c')
     print('Source path for ptv1.so is %s' % src_path)
     sys.path.append(src_path)
-    import ptv1 as ptv
     
     start = time.time()
     
@@ -210,11 +213,11 @@ def main(sys_argv, repetitions=1):
 
 
 if __name__ == '__main__':
-""" pyptv_batch.py enables to run a sequence without GUI
-    It can run from a command shell: 
+    """ pyptv_batch.py enables to run a sequence without GUI
+        It can run from a command shell: 
         python pyptv_batch.py ~/test_cavity 10000 10004
     
-    or from Python:
+        or from Python:
         
         import sys, os
         sys.path.append(os.path.abspath('openptv/openptv-python/pyptv_gui'))
@@ -223,7 +226,7 @@ if __name__ == '__main__':
         PyPTV_working_directory = '/openptv/Working_folder/'
         mi,mx = 65119, 66217
         main([batch_command,PyPTV_working_directory, mi, mx])
-"""
+        """
     if len(sys.argv) < 4:
         print("Wrong number of inputs, usage: python pyptv_batch.py \
         experiments/exp1 seq_first seq_last")
